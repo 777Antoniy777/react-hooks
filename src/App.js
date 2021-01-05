d43f1bfbc964e475a992ab27369b0ea27113f243
777Antoniy777 <tat7106@yandex.ru>
2021-01-05 23:01:51 +0600

    clear workspace

--- src/App.js
+++ src/App.js
@@ -1,51 +1,10 @@
-import React, {useState} from 'react';
+import React, {useEffect} from 'react';
 import './App.css';
 
-// if you need to set the calculation of some function
-// you can write it to useState like value 
-// const getRandomValue = () => {
-//   console.log('...calcilation');
-//   return +Math.random(0, 1).toFixed(2);
-// }
-
 function App() {
-  // const [counter, setCounter] = useState(() => getRandomValue());
-  const [counter, setCounter] = useState(0);
-  const [values, setValues] = useState({
-    title: 'Счетчик',
-    date: Date.now(),
-  });
-
-  const increaseValue = (evt) => {
-    evt.preventDefault();
-
-    setCounter(counter + 1);
-  };
-
-  const decreaseValue = (evt) => {
-    evt.preventDefault();
-
-    setCounter(counter - 1);
-  };
-
-  const changeTitle = (evt) => {
-    evt.preventDefault();
-
-    setValues({
-      ...values,
-      title: 'Новый счетчик',
-    });
-  }
-
   return (
     <div className="App">
-      <h1>Счетчик: {counter}</h1>
 
-      <button className="btn btn-danger" type="button" onClick={decreaseValue}>Уменьшить</button>
-      <button className="btn btn-success" type="button" onClick={increaseValue}>Увеличить</button>
-
-      <button className="btn btn-default" type="button" onClick={changeTitle}>Изменить заголовок</button>
-      <pre>{JSON.stringify(values)}</pre>
     </div>
   );
 }
