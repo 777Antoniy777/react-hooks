7aa936c5fd8f3f874f6b9495a3e1a7a01e3e0e37
Anton_Kuzmitsky <tat7106@gmail.com>
2021-01-07 14:02:51 +0600

    add comment

--- src/App.js
+++ src/App.js
@@ -1,51 +1,43 @@
-import React, {useState} from 'react';
+import React, {useEffect, useMemo, useState} from 'react';
 import './App.css';
 
-// if you need to set the calculation of some function
-// you can write it to useState like value 
-// const getRandomValue = () => {
-//   console.log('...calcilation');
-//   return +Math.random(0, 1).toFixed(2);
-// }
-
 function App() {
-  // const [counter, setCounter] = useState(() => getRandomValue());
-  const [counter, setCounter] = useState(0);
-  const [values, setValues] = useState({
-    title: 'Счетчик',
-    date: Date.now(),
-  });
+  const [number, setNumber] = useState(42);
+  const [colored, setColored] = useState(false);
 
-  const increaseValue = (evt) => {
-    evt.preventDefault();
+  // cash the object 
+  const titleStyle = useMemo(() => ({
+      color: colored ? "green" : "orange",
+  }), [colored]);
 
-    setCounter(counter + 1);
+  const complexCalculate = (num) => {
+    // some complex calculation
+    let i = 0;
+    while (i < 1000000000) i++;
+
+    return num * 2;
   };
 
-  const decreaseValue = (evt) => {
-    evt.preventDefault();
+  // cash complex calculation and function wont call if we change other state values 
+  const computedVal = useMemo(() => {
+    return complexCalculate(number);
+  }, [number]);
 
-    setCounter(counter - 1);
-  };
-
-  const changeTitle = (evt) => {
-    evt.preventDefault();
-
-    setValues({
-      ...values,
-      title: 'Новый счетчик',
-    });
-  }
+  // doesnt call because we use useMemo (s. 9)
+  useEffect(() => {
+    console.log('styles changed');
+    // titleStyle is object. all objects set on links
+    // became useEffect call every time when we rerender the component (prev link !== next link)
+    // and if we use useMemo we see that object and its link saved and rerender doesnt happen
+  }, [titleStyle]);
 
   return (
     <div className="App">
-      <h1>Счетчик: {counter}</h1>
+      <h1 style={titleStyle}>Вычисляемое свойство: {computedVal}</h1>
 
-      <button className="btn btn-danger" type="button" onClick={decreaseValue}>Уменьшить</button>
-      <button className="btn btn-success" type="button" onClick={increaseValue}>Увеличить</button>
-
-      <button className="btn btn-default" type="button" onClick={changeTitle}>Изменить заголовок</button>
-      <pre>{JSON.stringify(values)}</pre>
+      <button className="btn btn-danger" type="button" onClick={() => setNumber(number - 1)}>Уменьшить</button>
+      <button className="btn btn-success" type="button" onClick={() => setNumber(number + 1)}>Увеличить</button>
+      <button className="btn btn-warning" type="button" onClick={() => setColored(prev => !prev)}>Изменить</button>
     </div>
   );
 }
