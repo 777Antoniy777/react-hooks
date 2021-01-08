6d293fad1c3d56a1339ee7dfa6d22b5abca4955f
Anton_Kuzmitsky <tat7106@gmail.com>
2021-01-08 13:04:44 +0600

    add comment and compare the diference between useMemo and useCallback

--- src/App.js
+++ src/App.js
@@ -10,9 +10,15 @@ function App() {
     color: colored ? "green" : "orange",
   };
 
+  // useCallback uses for cashing all function 
+  // which set like callback in child component through props
+  // it prevents often rerender, because value will be saved and it s not a object link
   const generateItemsFromApi = useCallback(() => {
     return new Array(count).fill('').map((_, i) => `Елемент ${i + 1}`);
   }, [count]);
+
+  // show all function
+  console.log(generateItemsFromApi);
 
   return (
     <div className="App">
@@ -21,6 +27,8 @@ function App() {
       <button className="btn btn-success" type="button" onClick={() => setCount(count + 1)}>Увеличить</button>
       <button className="btn btn-warning" type="button" onClick={() => setColored(prev => !prev)}>Изменить</button>
 
+      {/* useMemo working in presents component, not in child! */}
+      {/* but if you caching primitive value you can use it too */}
       <ListItems getItems={generateItemsFromApi} />
     </div>
   );
