9e3766460a447dae4e498086beeb3ba72848f838
Anton_Kuzmitsky <tat7106@gmail.com>
2021-01-13 16:52:41 +0600

    learn custom hooks

--- src/App.js
+++ src/App.js
@@ -1,51 +1,60 @@
-import React, {useState} from 'react';
+import React, {useEffect, useState} from 'react';
 import './App.css';
 
-// if you need to set the calculation of some function
-// you can write it to useState like value 
-// const getRandomValue = () => {
-//   console.log('...calcilation');
-//   return +Math.random(0, 1).toFixed(2);
-// }
+const useLogger = (value) => {
+  useEffect(() => {
+    console.log(value);
+  }, [value]);
+};
+
+const useInput = (initialValue) => {
+  const [value, setValue] = useState(initialValue);
+
+  const onChange = (evt) => {
+    const target = evt.target;
+    const value = target.value;
+
+    setValue(value);
+  };
+
+  const clearValue = () => {
+    setValue('');
+  };
+
+  return {
+    arg: {
+      value,
+      onChange,
+    },
+    value,
+    clearValue,
+  };
+};
 
 function App() {
-  // const [counter, setCounter] = useState(() => getRandomValue());
-  const [counter, setCounter] = useState(0);
-  const [values, setValues] = useState({
-    title: 'Счетчик',
-    date: Date.now(),
-  });
+  const input = useInput('');
+  const email = useInput('');
 
-  const increaseValue = (evt) => {
-    evt.preventDefault();
+  useLogger(input.value);
+  useLogger(email.value);
 
-    setCounter(counter + 1);
+  const clear = () => {
+    input.clearValue();
+    email.clearValue();
   };
-
-  const decreaseValue = (evt) => {
-    evt.preventDefault();
-
-    setCounter(counter - 1);
-  };
-
-  const changeTitle = (evt) => {
-    evt.preventDefault();
-
-    setValues({
-      ...values,
-      title: 'Новый счетчик',
-    });
-  }
 
   return (
-    <div className="App">
-      <h1>Счетчик: {counter}</h1>
+    <div className="App container pt-3">
+      {/* We can use spread operator and set needle args for React element (example input here) */}
+      <input type="text" {...input.arg} />
+      <input type="email" {...email.arg} />
 
-      <button className="btn btn-danger" type="button" onClick={decreaseValue}>Уменьшить</button>
-      <button className="btn btn-success" type="button" onClick={increaseValue}>Увеличить</button>
+      <button className="btn btn-warning" type="reset" onClick={() => clear()}>Очистить</button>
 
-      <button className="btn btn-default" type="button" onClick={changeTitle}>Изменить заголовок</button>
-      <pre>{JSON.stringify(values)}</pre>
+      <h1>
+        <span style={{'paddingRight': '20px',}}>{input.value}</span>
+        <span>{email.value}</span>
+      </h1>
     </div>
   );
 }
